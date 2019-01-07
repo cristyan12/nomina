db95910b4ada00c8ff2ad7f2f5ddbd3c4d06f960
= <numenor21@gmail.com>
2019-01-07 17:24:59 -0400

    Fixs

--- resources/views/contracts/partials/form.blade.php
+++ resources/views/contracts/partials/form.blade.php
@@ -1,8 +1,16 @@
 <div class="form-group row">
-    {{ Form::label('type', 'Tipo del contrato:', ['class' => 'col-sm-3 col-form-label']) }}
+    {{ Form::label('type', 'Tipo:', ['class' => 'col-sm-3 col-form-label']) }}
 
     <div class="col-md-4">
-        {{ Form::text('type', null, ['class' => 'form-control', 'placeholder' => 'Temporal']) }}
+        {{ Form::text('type', null, ['class' => 'form-control', 'placeholder' => 'Tipo del contrato']) }}
+    </div>
+</div>
+
+<div class="form-group row">
+    {{ Form::label('duration', 'Duración:', ['class' => 'col-sm-3 col-form-label']) }}
+
+    <div class="col-md-9">
+        {{ Form::text('duration', null, ['class' => 'form-control', 'placeholder' => 'Duración del contrato']) }}
     </div>
 </div>
 
