f837e7d626c7ea61c711da5a1c5706d444866b0b
Cristyan Valera <numenor21@gmail.com>
2019-02-11 18:44:44 -0400

    reorder methods in EmployeeProfile

--- app/EmployeeProfile.php
+++ app/EmployeeProfile.php
@@ -8,14 +8,14 @@ class EmployeeProfile extends Model
 {
     protected $guarded = [];
 
-    public function department()
-    {
-    	return $this->belongsTo(Department::class);
-    }
-
     public function employees()
     {
     	return $this->hasMany(Employee::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
     }
 
     public function position()
