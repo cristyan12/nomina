d8cb88fe4d1b4cc987b2c3f0d03f5dd95f8790fc
Cristyan Valera <numenor21@gmail.com>
2019-10-15 17:59:20 -0400

    add format(d-m-Y) to date fields pf nomina model

--- app/Nomina.php
+++ app/Nomina.php
@@ -9,7 +9,7 @@ class Nomina extends Model
     protected $table = 'nominas';
 
     protected $fillable = [
-        'name', 'type', 'periods', 
+        'name', 'type', 'periods',
         'first_period_at', 'last_period_at'
     ];
 
@@ -35,7 +35,7 @@ class Nomina extends Model
     public function getFirstDatePeriodAttribute()
     {
         if (! $this->first_period_at == '' ) {
-            return $this->first_period_at->toDateString();
+            return $this->first_period_at->format('d-m-Y');
         }
 
         return 'N/D';
@@ -44,7 +44,7 @@ class Nomina extends Model
     public function getLastDatePeriodAttribute()
     {
         if (! $this->last_period_at == '' ) {
-            return $this->last_period_at->toDateString();
+            return $this->last_period_at->format('d-m-Y');
         }
 
         return 'N/D';
