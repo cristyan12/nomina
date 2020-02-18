73c71359cc3d33c9cf19324660310c5f60e2ac30
Cristyan Valera <numenor21@gmail.com>
2020-02-18 19:51:52 -0400

    formula p/ calcular las horas de bonif. T. Viaje Nocturno

--- app/CalculatePay.php
+++ app/CalculatePay.php
@@ -14,6 +14,12 @@ trait CalculatePay
         '83' => 1.83,
     ];
     protected $percentTravelTime;
+    protected $daysWorkedDay;
+    protected $mixedDaysWorked;
+    protected $sixthDayWorkedDay;
+    protected $sixthDayWorkedMixed;
+    protected $nightWorkedDays;
+    protected $sixthDayWorkedNigth;
 
     public function setDaysWorked(int $days): self
     {
@@ -29,11 +35,61 @@ trait CalculatePay
         return $this;
     }
 
-    public function setPercentTravelTime($percent): self
+    public function setPercentTravelTime(float $percent): self
     {
         $this->percentTravelTime = $this->percentsTravelTime[$percent];
 
         return $this;
+    }
+
+    public function setDaysWorkedDay(int $days): self
+    {
+        $this->daysWorkedDay = $days;
+
+        return $this;
+    }
+
+    public function setSixthDayWorkedDay(int $day): self
+    {
+        $this->sixthDayWorkedDay = $day;
+
+        return $this;
+    }
+
+    public function setMixedDaysWorked(int $days): self
+    {
+        $this->mixedDaysWorked = $days;
+
+        return $this;
+    }
+
+    public function setSixthDayWorkedMixed(int $day): self
+    {
+        $this->sixthDayWorkedMixed = $day;
+
+        return $this;
+    }
+
+    public function setNightWorkedDays(int $days): self
+    {
+        $this->nightWorkedDays = $days;
+
+        return $this;
+    }
+
+    public function setSixthDayWorkedNigth(int $day): self
+    {
+        $this->sixthDayWorkedNigth = $day;
+
+        return $this;
+    }
+
+    public function getBonusTravelTimeNight()
+    {
+        // =((DT Diurnos + 6DT Diurno) * 0,5) + ((DT Mixtos + 6DT Mixto) * 1,5) + ((DT Noct + 6DT Noct) * 1,5)
+        return (($this->daysWorkedDay + $this->sixthDayWorkedDay) * 0.5) +
+            (($this->mixedDaysWorked + $this->sixthDayWorkedMixed) * 1.5) +
+            (($this->nightWorkedDays + $this->sixthDayWorkedNigth) * 1.5);
     }
 
     public function daysWorked(): float
--- tests/Unit/CalculatePayTest.php
+++ tests/Unit/CalculatePayTest.php
@@ -12,7 +12,7 @@ use Tests\TestCase;
 
 class CalculatePayTest extends TestCase
 {
-    use DatabaseTransactions;
+    use RefreshDatabase;
 
     /** @test */
     function it_calculate_5_days_worked()
@@ -59,9 +59,32 @@ class CalculatePayTest extends TestCase
         $this->assertSame($travelTime, 1151.6062499999998);
     }
 
+    // Bonif. por tiempo de viaje nocturno
+
+    /** @test */
+    function it_get_the_quantities_bonus_for_travel_time_night()
+    {
+        $employee = $this->create(EmployeeProfile::class);
+
+
+        $quantity = $employee
+            ->setDaysWorkedDay(2)
+            ->setSixthDayWorkedDay(0)
+            ->setMixedDaysWorked(0)
+            ->setSixthDayWorkedMixed(0)
+            ->setNightWorkedDays(4)
+            ->setSixthDayWorkedNigth(0)
+            ->getBonusTravelTimeNight();
+
+        $this->assertEquals(7, $quantity);
+    }
+
     /** @test */
     function it_calculate_bonus_per_worked_in_sunday()
     {
+        $this->markTestIncomplete();
+        return;
+
         $position = $this->create(Position::class, ['basic_salary' => 1735.00]);
         $employee = $this->create(EmployeeProfile::class, ['position_id' => $position->id]);
         $employee->setDaysWorked(5)
