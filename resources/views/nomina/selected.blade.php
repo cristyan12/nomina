01d0f8332c84247bd342a7cce791f73b9a01d0a2
Cristyan Valera <numenor21@gmail.com>
2020-06-10 23:07:55 -0400

    wip

--- resources/views/nomina/selected.blade.php
+++ resources/views/nomina/selected.blade.php
@@ -10,57 +10,55 @@
         <table class="table table-bordered table-hover table-striped">
             <thead>
             <tr>
-                <th>Código</th>
                 <th>Cédula</th>
                 <th>Nombre</th>
                 <th>Departamento</th>
                 <th>Cargo</th>
-                <th>Salario Base</th>
-                <th colspan="1">Horas</th>
-                <th colspan="1">Jornada</th>
-                <th colspan="1">TViaje</th>
+                <th>Salario base</th>
+                <th>HExtras</th>
+                <th>Jornada</th>
+                <th>TViaje</th>
                 <th>&nbsp;</th>
             </tr>
             </thead>
             <tbody>
-            @foreach($nomina->employees as $employee)
-            <tr>
-                <th width="10px">{{ $employee->document }}</th>
-                <td width="10px">{{ $employee->code }}</td>
-                <td>{{ str_limit($employee->full_name, 20) }}</td>
-                <td>{{ str_limit($employee->profile->department->name, 20) }}</td>
-                <td>{{ str_limit($employee->profile->position->name, 10) }}</td>
-                <td>{{ $employee->profile->position->format_salary }}</td>
-                <form action="">
-                    <div class="form-group col-md-2">
-                        <td width="20px">
-                            <input class="form-control form-control-sm" type="text" name="hours">
-                        </td>
-                    </div>
+                @foreach($nomina->employees as $employee)
+                    <tr>
+                        <th width="10px">{{ $employee->document }}</th>
+                        <td>{{ str_limit($employee->full_name, 23) }}</td>
+                        <td>{{ str_limit($employee->profile->department->name, 20) }}</td>
+                        <td>{{ str_limit($employee->profile->position->name, 10) }}</td>
+                        <td>{{ $employee->profile->position->format_salary }}</td>
+                        <form action="">
+                            <div class="form-group col-md-2">
+                                <td width="10px">
+                                    <input class="form-control form-control-sm" type="text" name="hours">
+                                </td>
+                            </div>
 
-                    <div class="form-group col-md-2">
-                        <td width="20px">
-                            <select class="custom-select custom-select-sm" name="journal">
-                                <option value="">...</option>
-                                <option value="d">D - Diurna</option>
-                                <option value="m">M - Mixta</option>
-                                <option value="n">N - Nocturna</option>
-                            </select>
-                        </td>
-                    </div>
+                            <div class="form-group col-md-2">
+                                <td width="10px">
+                                    <select class="custom-select custom-select-sm" name="journal">
+                                        <option value="">...</option>
+                                        <option value="d">D - Diurna</option>
+                                        <option value="m">M - Mixta</option>
+                                        <option value="n">N - Nocturna</option>
+                                    </select>
+                                </td>
+                            </div>
 
-                    <div class="form-group col-md-2">
-                        <td width="20px">
-                            <input class="form-control form-control-sm" type="text" name="travel">
-                        </td>
-                    </div>
-                </form>
+                            <div class="form-group col-md-2">
+                                <td width="20px">
+                                    <input class="form-control form-control-sm" type="text" name="travel">
+                                </td>
+                            </div>
 
-                <td>
-                    <button type="submit" class="btn btn-primary btn-sm">Grabar</button>
-                </td>
-            </tr>
-            @endforeach
+                            <td width="10px">
+                                <button type="submit" class="btn btn-primary btn-sm">Grabar</button>
+                            </td>
+                        </form>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
